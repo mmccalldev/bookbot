f890c15eb43dbe1d321376911c2bcaf56c9c87e5
Michael McCall <141757564+mmccalldev@users.noreply.github.com>
2024-11-07 16:19:18 -0600

    add functions to allow the program to read text from file_path

---
+++ main.py
@@ -0,0 +1,28 @@
+def main():
+    file_path = 'books/frankenstein.txt'
+    output_text(file_path) #call the output_text function to see if the user wants to output the text from the book
+    text = read_text(file_path) #assign the 'text' variable as the output of the read_text function
+    word_count = get_word_count(text) #assign the 'word_count' variable to the output of the 'get_word_count' function
+    print(f"The word count for the text is: {word_count}")
+
+def get_word_count(text):
+    words = text.split()
+    return len(words)
+
+def read_text(file_path):
+    with open(file_path) as f:
+        text = f.read()
+    return text
+
+def output_text(file_path):
+    read_text_input = input("Would you like to output the text from the book into the console? (y/n): ")
+    if read_text_input in ('y', 'Y'):
+        print(read_text(file_path))
+    elif read_text_input in ('n', 'N'):
+        pass
+    else:
+        print("Sorry that is not a valid input, please try again.")
+        return output_text(file_path)
+
+main()
+
