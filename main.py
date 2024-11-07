5542a171e9ebd02cba015e96de95df122c51fbfa
Michael McCall <141757564+mmccalldev@users.noreply.github.com>
2024-11-07 16:39:20 -0600

    add get_char_count function to count all of the characters in text

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main():
     text = read_text(file_path) #assign the 'text' variable as the output of the read_text function
     word_count = get_word_count(text) #assign the 'word_count' variable to the output of the 'get_word_count' function
     print(f"The word count for the text is: {word_count}")
+    get_char_count(file_path)
 
 def get_word_count(text):
     words = text.split()
@@ -17,12 +18,25 @@ def read_text(file_path):
 def output_text(file_path):
     read_text_input = input("Would you like to output the text from the book into the console? (y/n): ")
     if read_text_input in ('y', 'Y'):
-        print(read_text(file_path))
+        print(f'{read_text(file_path)}\n')
     elif read_text_input in ('n', 'N'):
         pass
     else:
         print("Sorry that is not a valid input, please try again.")
         return output_text(file_path)
+
+def get_char_count(file_path):
+    character_dict = {}
+    words = read_text(file_path).lower()
+    for char in words:
+        if char not in character_dict:
+            character_dict[char] = 1
+        else:
+            character_dict[char] += 1
+    print(character_dict)
+
+
+
 
 main()
 
